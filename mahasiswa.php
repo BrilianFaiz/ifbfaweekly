ca05521564e3f5dee59630367571f13c3b00e0d0
brilian <brilianfaiz734@gmail.com>
2026-05-27 16:04:09 +0700

    menyambungkan database dan menampilkan data di table

---
+++ mahasiswa.php
@@ -0,0 +1,97 @@
+<?php
+require_once './connections.php';
+
+// query ambil data mahasiswa
+$query = mysqli_query($conn, "SELECT * FROM mahasiswa");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+
+<body>
+    <h1>WEB Informatika</h1>
+    <hr>
+    <table border="1" cellspacing="0" cellpadding="10px">
+        <tr>
+            <td><a href="index.php">Home</a></td>
+            <td><a href="profile.php">Profile</a></td>
+            <td><a href="contact.php">Contact</a></td>
+            <td><a href="mahasiswa.php">Data Mahasiswa</a></td>
+        </tr>
+    </table>
+    <h2>DATA MAHASISWA</h2>
+    <hr>
+    <table border="1" cellpadding="5px">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Jurusan</th>
+            <th>Email</th>
+            <th>No HP</th>
+            <th>Foto</th>
+        </tr>
+        <?php
+        $no = 1;
+
+        while ($row = mysqli_fetch_assoc($query)):
+            ?>
+            <tr>
+                <td align="center">
+                    <?= $no++; ?>
+                </td>
+
+                <td>
+                    <?= $row['nama']; ?>
+                </td>
+
+                <td>
+                    <?= $row['nim']; ?>
+                </td>
+
+                <td>
+                    <?= $row['jurusan']; ?>
+                </td>
+
+                <td>
+                    <?= $row['email']; ?>
+                </td>
+
+                <td>
+                    <?= $row['no_hp']; ?>
+                </td>
+
+                <td align="center">
+                    <img src="assets/images/<?= $row['foto']; ?>" width="120px">
+                </td>
+            </tr>
+
+        <?php endwhile; ?>
+    </table>
+    <br>
+    <hr>
+    <a href="inputdata.php"><button>input data</button></a>
+    <!-- <table border="1">
+        <tr>
+            <th>1,1</th> <th>1,2</th> <th>1,3</th> <th>1,4</th>
+        </tr>
+        <tr>
+            <th>2,1</th><th colspan="2" rowspan="2">?</th> <th>2,4</th>
+        </tr>
+        <tr>
+            <th>3,1</th> <th>3,4</th>
+        </tr>
+        <tr>
+            <th>4,1</th> <th>4,2</th> <th>4,3</th> <th>4,4</th>
+        </tr>
+    </table> -->
+
+</body>
+
+</html>
--- profile.php
+++ profile.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact</title>
+    <title>Profile</title>
 </head>
 <body>
     <table border="1" cellspacing = "0" cellpadding = "10px">
   <tr>
-    <td><a href="index.html">Home</a></td>
-    <td><a href="profile.html">Profile</a></td>
-    <td><a href="contact.html">Contact</a></td>
-    <td>Data Mahasiswa</td>
+    <td><a href="index.php">Home</a></td>
+    <td><a href="profile.php">Profile</a></td>
+    <td><a href="contact.php">Contact</a></td>
+    <td><a href="mahasiswa.php">Data Mahasiswa</a></td>
   </tr>
-</table>
+</table>    
 </body>
 </html>
